4695577215d715d86ecdecfc98bb2b0f5ef25e3f
bhanu chowdary <bhanuphani100@gmail.com>
2020-10-08 11:08:44 +0530

    error page

--- frontend/HR-Panel/src/app/app-routing.module.ts
+++ frontend/HR-Panel/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { ResultComponent } from './components/result/result.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'results', component: ResultComponent}
+  {path: 'results', component: ResultComponent},
+  {path: '404', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
